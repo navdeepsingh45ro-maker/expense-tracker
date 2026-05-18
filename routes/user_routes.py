d3de00f5368200b73f136c595ad2ccce0f421bdf
navdeepsingh45ro-maker <navdeepsingh45ro@gmail.com>
2026-05-18 18:10:34 +0530

    Completed user authentication with JWT

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -1,8 +1,15 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 from schemas.user_schema import UserCreate
 from models.user_model import User
 from database import SessionLocal
 from auth.hashing import hash_password
+from schemas.login_schema import LoginSchema
+from auth.hashing import verify_password
+from auth.jwt_handler import create_access_token
+from sqlalchemy.orm import Session
+from database import get_db
+ 
+
 
 router = APIRouter()
 @router.post("/users/")
@@ -18,3 +25,21 @@ def create_user(user: UserCreate):
     db.add(new_user)
     db.commit()
     return {"message": "User created successfully"}
+
+@router.post("/login")
+def login(request: LoginSchema, db: Session = Depends(get_db)):
+
+    user = db.query(User).filter(User.email == request.email).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    if not verify_password(request.password, user.password):
+        raise HTTPException(status_code=401, detail="Incorrect password")
+
+    access_token = create_access_token(data={"user_id": user.id})
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
